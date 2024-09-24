24ec076990a75216eb7ccc52a133c4d1722fa930
imad-U-Din <imaadtariq17@gmail.com>
2024-09-24 18:10:03 +0530

    36.c

--- 36.c
+++ 36.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
-    int i, j;
-    int rows = 4; 
+int main() {
+    int rows = 4;
 
-    for (i = 1; i <= rows; i++)
-    {
-        for (j = i; j < rows; j++)
-        {
+    for (int i = 1; i <= rows; i++) {
+        for (int j = rows - i; j >= 1; j--) {
             printf(" ");
         }
-        for (j = 1; j <= (2 * i - 1); j++)
-        {
-            printf("*");
+        for (int k = 1; k <= i; k++) {
+            printf("* ");
         }
         printf("\n");
     }
